e00b1775752b885869a1f31ea1770aa13684b5e1
Dima <dimabolnik@mail.ru>
2025-02-17 14:55:03 +0300

    "First commit"

---
+++ src/main/java/ru/dev/bolnik/sprint1/Main.java
@@ -0,0 +1,38 @@
+package ru.dev.bolnik.sprint1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+//        byte a = 10;
+//        byte b = 20;
+//        System.out.println(findMax(a, b));
+//        int count = 8;
+//        for (int i = 0; i < 7; i++) {
+//            System.out.println(--count);
+//        }
+//        int x = 14;
+//        int y = 20;
+//        int z = x % y;
+//        System.out.println(z);
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        Set<String> strings = map.keySet();
+        for (String str : map.values()) {
+            System.out.println(str);
+        }
+        System.out.println(map);
+    }
+
+    private static byte findMax(byte a, byte b) {
+        return (byte) Integer.max(a, b);
+    }
+}
